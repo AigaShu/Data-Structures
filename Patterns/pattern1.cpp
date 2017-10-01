352e35a766a1f794cf153ebd99a1bef462abfba7
AigaShu <32430332+AigaShu@users.noreply.github.com>
2017-10-01 13:36:24 +0200

    Update pattern1.cpp with comments
    Add coments to make the  understanding easier

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(){
 
-	int n;
-	cin >> n;
+	int n; // This set "n" as a whole number
+	cin >> n; // This  let you set "n" as  parameter. For example: n = 2
 
-	for( int i = 0; i < n; i++ ){
-		for( int j = 0; j < n; j++ ){
-			cout << "*";
+	for( int i = 0; i < n; i++ ){ //The program will execute if this parameters are true
+		for( int j = 0; j < n; j++ ){ //And this , too
+			cout << "*"; //And this will show you the result
 		}
 		cout << endl;
 	}
-    return 0;
+    return 0; //This finish the app
 
 }
 
